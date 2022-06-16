1b8df58fdcc3fe2ef14ed89415d63ed318558747
keshkayode71@gmail.com <keshkayode71@gmail.com>
2022-06-16 02:22:41 +0100

    This solves Leetcode Question #442. it asks to remove the duplicates and append the new collection  in a new array.

---
+++ Dup.licate.js
@@ -0,0 +1,19 @@
+let array = [4, 3, 2, 7, 8, 2, 3, 1];
+let len = array.length;
+const x = function DuplicatesOut(nums) {
+  let result = [];
+
+  for (let i = 0; i < len; i++) {
+    let value = Math.abs(nums[i]) - 1;
+    if (nums[value] < 0) {
+      result.push(value + 1)
+    } else {
+      nums[value] *= -1;
+    }
+  }
+  return result;
+}
+
+ console.log(x(array));
+
+ 
